7a03d91b350eb03314ecbf1cae8069f751270420
nmusso <niccolo.mussoni@gmail.com>
2022-12-16 21:09:24 +0100

    switch between left and bottom

--- website/template/base.php
+++ website/template/base.php
@@ -8,7 +8,7 @@
 	<!-- Bootstrap CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
 		integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="../css/style.css">
 
 	<title>iSocial</title>
 </head>
@@ -22,31 +22,38 @@
 				</header>
 			</div>
 		</div>
-		<main>
-		</main>
-		<div class="row fixed-bottom">
-			<div class="col-md-1"></div>
-			<div class="col-12 col-md-10">
-				<ul class="nav nav-pills bg-dark">
-					<li class="nav-item col col-md-2">
-						<a class="nav-link bg-dark text-white text-center mt-2 mx-2" href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
-					</li>
-					<li class="nav-item col col-md-2">
-						<a class="nav-link bg-dark text-white text-center mt-2 mx-2" href="#"><i class="fa-solid fa-circle-plus"></i></a>
-					</li>
-					<li class="nav-item col col-md-2">
-						<a class="nav-link bg-dark text-white text-center mt-2 mx-2" href="#"><i class="fa-solid fa-house"></i></a>
-					</li>
-					<li class="nav-item col col-md-2">
-						<a class="nav-link bg-dark text-white text-center mt-2 mx-2" href="#"><i class="fa-solid fa-bell"></i></a>
-					</li>
-					<li class="nav-item col col-md-2">
-						<a class="nav-link bg-dark text-white text-center mt-2 mx-2" href="#"><i class="fa-solid fa-circle-user"></i></a>
-					</li>
-				</ul>
+		<div class="row">
+			<div class="col-4">
+				<div id="nav" class="row">
+					<div class="col-md-1"></div>
+					<div class="col-12 col-md-10 bg-dark">
+						<ul class="nav nav-pills">
+							<li class="nav-item col col-md-2">
+								<a class="nav-link bg-dark text-white text-center mt-2 mx-2" href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
+							</li>
+							<li class="nav-item col col-md-2">
+								<a class="nav-link bg-dark text-white text-center mt-2 mx-2" href="#"><i class="fa-solid fa-circle-plus"></i></a>
+							</li>
+							<li class="nav-item col col-md-2">
+								<a class="nav-link bg-dark text-white text-center mt-2 mx-2" href="#"><i class="fa-solid fa-house"></i></a>
+							</li>
+							<li class="nav-item col col-md-2">
+								<a class="nav-link bg-dark text-white text-center mt-2 mx-2" href="#"><i class="fa-solid fa-bell"></i></a>
+							</li>
+							<li class="nav-item col col-md-2">
+								<a class="nav-link bg-dark text-white text-center mt-2 mx-2" href="#"><i class="fa-solid fa-circle-user"></i></a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-md-1"></div>
+				</div>
 			</div>
-			<div class="col-md-1"></div>
+			<div class="col-8">
+				<main>
+				</main>
+			</div>
 		</div>
+		
 	</div>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
 		integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
