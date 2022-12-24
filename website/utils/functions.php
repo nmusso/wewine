04588c3564c52b41c639cf5ba3fa9ac4ad79817b
nmusso <niccolo.mussoni@gmail.com>
2022-12-24 10:52:33 +0100

    fix sec_session_start (secure login working)

--- website/utils/functions.php
+++ website/utils/functions.php
@@ -12,12 +12,27 @@ function sec_session_start() {
     $session_name = 'sec_session_id'; // Imposta un nome di sessione
     $secure = false; // Imposta il parametro a true se vuoi usare il protocollo 'https'.
     $httponly = true; // Questo impedirà ad un javascript di essere in grado di accedere all'id di sessione.
-    ini_set('session.use_only_cookies', 1); // Forza la sessione ad utilizzare solo i cookie.
     $cookieParams = session_get_cookie_params(); // Legge i parametri correnti relativi ai cookie.
-    session_set_cookie_params($cookieParams["lifetime"], $cookieParams["path"], $cookieParams["domain"], $secure, $httponly); 
-    session_name($session_name); // Imposta il nome di sessione con quello prescelto all'inizio della funzione.
-    session_start(); // Avvia la sessione php.
-    session_regenerate_id(); // Rigenera la sessione e cancella quella creata in precedenza.
+
+    if(isset($_SESSION)) {
+        if ($cookieParams["lifetime"] != 0) {
+            setcookie(session_name(), session_id(), time() + $cookieParams["lifetime"]);
+        } else {
+            setcookie(session_name(), session_id(), $cookieParams["lifetime"]);
+        }
+    } else {
+        ini_set('session.use_only_cookies', 1); // Forza la sessione ad utilizzare solo i cookie.
+        session_name($session_name); // Imposta il nome di sessione con quello prescelto all'inizio della funzione.
+        session_set_cookie_params(
+            $cookieParams["lifetime"],
+            $cookieParams['path'],
+            $cookieParams['domain'],
+            $secure,
+            $httponly
+        );
+        session_regenerate_id(); // Rigenera la sessione e cancella quella creata in precedenza.
+        session_start(); // Avvia la sessione php.
+    }    
 }
 
 
