46fec1529010b88090b07c57fe80fb9c059dc234
Alessandro Sciarrillo <alessandr.sciarrill2@studio.unibo.it>
2023-01-19 17:00:43 +0100

    commnet and like frontend in test

--- website/template/test.php
+++ website/template/test.php
@@ -65,22 +65,145 @@
 			<div id="mainSection" class="col-12 col-sm-10">
 				<main>
 					<!-- TEST -->
-					<div class="row">
-						<div class="col-12 col-sm-12">
-							<div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-								<div class="toast-header">
-									<img src="../upload/io.jpg" class="rounded me-2" alt="...">
-									<strong class="me-auto">Bootstrap</strong>
-									<small>11 mins ago</small>
-									<button type="button" class="btn-close" data-bs-dismiss="toast"
-										aria-label="Close"></button>
+					<div class="row mt-3">
+						<div class="col-sm-1"></div>
+						<div class="col-12 col-sm-8 card">
+							<div class="card-body">
+								<a href="profile.php?profile=0`">
+									<div class="row">
+										<div class="col-2 col-sm-2 col-md-3 col-lg-3 col-xl-1">
+											<img src="../upload/9_propic.jpg"
+												class="img-fluid rounded-circle img-thumbnail p-1 propic" />
+										</div>
+										<div class="col-10 col-sm-10 col-md-9 col-lg-9 col-xl-11">
+											<div class="card-body p-0">
+												<p class="card-text">aleciro</p>
+											</div>
+										</div>
+									</div>
+								</a>
+								<p class="card-text my-3">Questo è un post</p>
+								<p class="card-text"><small class="text-muted">Posted 10 minutes ago</small></p>
+								<img class="card-img-bottom" src="../upload/9_1.jpeg" alt="Card image cap">				
+								<div class="row pt-3 pr-1 mb-1">
+									<div class="col-10 col-sm-10"></div>
+									<div class="col-1 col-sm-1">
+										<i class="fa-solid fa-heart"></i>
+									</div>
+									<div class="col-1 col-sm-1">
+										<i class="fa-regular fa-comment"></i>
+									</div>
 								</div>
-								<div class="toast-body">
-									Hello, world! This is a toast message.
+
+								<div class="row mt-3 m-0">
+									<div class="col-12 col-sm-12 p-0">
+										<div class="card border-0 p-0">
+											<div class="row m-0">
+												<div class="col-11 col-sm-11 p-0">
+													<div class="d-flex form-inputs">
+														<input id="searchBar" class="form-control" type="text" placeholder="Search any profile...">
+														<i class="bx bx-search"></i>
+													</div>
+												</div>
+												<div class="col-1 col-sm-1 p-0">
+													<button id="but" type="button" class="btn btn-dark">
+														<i class="fas fa-search"></i>
+													</button>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="row  mt-1">
+									<div class="col-12 col-sm-12 p-0">
+										<div class="card mx-2 px-2">
+											<div class="row">
+												<div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-1 p-2">
+													<img src="../upload/9_propic.jpg" class="img-fluid rounded-circle img-thumbnail p-1 propic" />
+												</div>
+												<div class="col-10 col-sm-10 col-md-10 col-lg-10 col-xl-11">
+													<div class="row">
+														<div class="card-body col-9 col-sm-9 col-md-9 col-lg-9 col-xl-9">
+															<p class="card-text">aleciro</p>
+														</div>
+													</div>
+													<div class="row mb-2">
+														<div class="col-12 col-sm-12">
+															Ti ho commentato la foto e ha detto che è porprio tanto tanto tanto bella.
+															Ciao da dallla cane ciro.
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div class="row  mt-1">
+									<div class="col-12 col-sm-12 p-0">
+										<div class="card mx-2 px-2">
+											<div class="row">
+												<div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-1 p-2">
+													<img src="../upload/9_propic.jpg" class="img-fluid rounded-circle img-thumbnail p-1 propic" />
+												</div>
+												<div class="col-10 col-sm-10 col-md-10 col-lg-10 col-xl-11">
+													<div class="row">
+														<div class="card-body col-9 col-sm-9 col-md-9 col-lg-9 col-xl-9">
+															<p class="card-text">aleciro</p>
+														</div>
+													</div>
+													<div class="row mb-2">
+														<div class="col-12 col-sm-12">
+															Ti ho commentato la foto e ha detto che è porprio tanto tanto tanto bella.
+															Ciao da dallla cane ciro.
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+						<div class="col-sm-3"></div>
+					</div>
+
+					<div class="row mt-3">
+						<div class="col-sm-1"></div>
+						<div class="col-12 col-sm-8 card">
+							<div class="card-body">
+								<a href="profile.php?profile=0`">
+									<div class="row">
+										<div class="col-2 col-sm-2 col-md-3 col-lg-3 col-xl-1">
+											<img src="../upload/9_propic.jpg"
+												class="img-fluid rounded-circle img-thumbnail p-1 propic" />
+										</div>
+										<div class="col-10 col-sm-10 col-md-9 col-lg-9 col-xl-11">
+											<div class="card-body p-0">
+												<p class="card-text">aleciro</p>
+											</div>
+										</div>
+									</div>
+								</a>
+								<p class="card-text my-3">Questo è un post</p>
+								<p class="card-text"><small class="text-muted">Posted 10 minutes ago</small></p>
+								<img class="card-img-bottom" src="../upload/9_2.png" alt="Card image cap">
+								<div class="row pt-3 pr-1">
+									<div class="col-10 col-sm-10"></div>
+									<div class="col-1 col-sm-1">
+										<i class="fa-regular fa-heart"></i>
+									</div>
+									<div class="col-1 col-sm-1">
+										<i class="fa-regular fa-comment"></i>
+									</div>
 								</div>
 							</div>
 						</div>
+						<div class="col-sm-3"></div>
 					</div>
+
 					<!-- TEST -->
 				</main>
 			</div>
