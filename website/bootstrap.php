f1b501d867fd6b10ade6b82279d1b8fe259ec639
Alessandro Sciarrillo <alessandr.sciarrill2@studio.unibo.it>
2023-01-19 18:38:48 +0100

    added secondary online db connection

--- website/bootstrap.php
+++ website/bootstrap.php
@@ -3,5 +3,10 @@ session_start();
 define("UPLOAD_DIR", "./upload/");
 require_once("utils/functions.php");
 require_once("db/database.php");
+
+// LOCAL
 $dbh = new DatabaseHelper("localhost", "sec_user", "f&4@YXuC9$5C^nLQ", "social", 3306);
+// HOSTED
+//$dbh = new DatabaseHelper("sql11.freesqldatabase.com", "sql11591817", "K9lnnXrFuQ", "sql11591817", 3306);
+
 ?>
