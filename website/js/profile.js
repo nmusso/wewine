d9cb610bb4eb1a27275a78a792299577b1418bd6
nmusso <niccolo.mussoni@gmail.com>
2023-01-20 22:27:15 +0100

    logout frontend ok

--- website/js/profile.js
+++ website/js/profile.js
@@ -6,27 +6,27 @@ function generaPost(data) {
             <div id="` + data["idPost"] + `" class="card-body">
                 <div class="row">
                     <div class="col-2 col-sm-2 col-md-3 col-lg-3 col-xl-1">
-                        <img src="`+data["imgProfilo"]+`"
+                        <img src="`+ data["imgProfilo"] + `"
                             class="img-fluid rounded-circle img-thumbnail p-1 propic" />
                     </div>
                     <div class="col-10 col-sm-10 col-md-9 col-lg-9 col-xl-11">
                         <div class="card-body p-0">
-                            <p class="card-text">`+data["username"]+`</p>
+                            <p class="card-text">`+ data["username"] + `</p>
                         </div>
                     </div>
                 </div>
-                <p class="card-text my-3">`+data["testo"]+`</p>
-                <p class="card-text"><small class="text-muted">Posted `+data["diffTime"]+`ago</small></p>
-                <img class="card-img-bottom" src="`+data["immagine"]+`" alt="Card image cap">
+                <p class="card-text my-3">`+ data["testo"] + `</p>
+                <p class="card-text"><small class="text-muted">Posted `+ data["diffTime"] + `ago</small></p>
+                <img class="card-img-bottom" src="`+ data["immagine"] + `" alt="Card image cap">
                 <div class="row pt-3 pr-1 mb-1">
                     <div class="col-10 col-sm-10"></div>      
                     <div class="col-1 col-sm-1">
-                        <a href="#!" onclick="likeChange(`+data["idPost"]+`)">
-                            <i class="`+data["liked"]+` fa-heart" id="idLike-`+data["idPost"]+`"></i>
+                        <a href="#!" onclick="likeChange(`+ data["idPost"] + `)">
+                            <i class="`+ data["liked"] + ` fa-heart" id="idLike-` + data["idPost"] + `"></i>
                         </a>
                     </div>
                     <div class="col-1 col-sm-1">
-                        <a href="#!" onclick="commentManager(`+data["idPost"]+`)">
+                        <a href="#!" onclick="commentManager(`+ data["idPost"] + `)">
                             <i class="fa-regular fa-comment"></i>
                         </a>
                     </div>  
@@ -66,13 +66,28 @@ function generaRiga(user) {
 }
 
 function generaUtente(data, isMine) {
-    let buttonContent = "";
+    let buttonRow = "";
+
     if (isMine) {
-        buttonContent = buttonEdit + buttonLogout;
+        buttonRow = `
+        <div id="buttonArea" class="row mt-3">
+            <div class="col-md-5 col-lg-7"></div>
+            ` + buttonEdit 
+            + ` <div class="col-md-1 col-lg-1"></div>`
+            + buttonLogout + `
+        </div>`
     } else if (data["isFollowing"][0]["isFollowing"] == 0) {
-        buttonContent = buttonFollow;
+        buttonRow = `
+        <div id="buttonArea" class="row mt-3">
+            <div class="col-md-9 col-lg-10"></div>
+            ` + buttonFollow + `
+        </div>`
     } else {
-        buttonContent = buttonUnfollow;
+        buttonRow = `
+        <div id="buttonArea" class="row mt-3">
+            <div class="col-md-9 col-lg-10"></div>
+            ` + buttonUnfollow + `
+        </div>`
     }
 
     let profile = `
@@ -144,11 +159,7 @@ function generaUtente(data, isMine) {
                         </div>
                     </div>
                 </div>
-
-                <div id="buttonArea" class="row mt-3">
-                    <div class="col-md-10 col-lg-10"></div>
-                    ` + buttonContent + `
-                </div>
+                ` + buttonRow + `
             </div>
         </div>
         <div class="col-sm-3"></div>
@@ -158,8 +169,8 @@ function generaUtente(data, isMine) {
     return profile;
 }
 
-const buttonEdit = `<button id="btnedit" type="button" onclick="edit()" class="btn btn-dark col-12 col-sm-12 col-md-1 col-lg-1">Edit</button>`;
-const buttonLogout = `<button id="btnlogout" type="button" onclick="logout()" class="btn btn-dark col-12 col-sm-12 col-md-1 col-lg-1">Logout</button>`;
+const buttonEdit = `<button id="btnedit" type="button" onclick="edit()" class="mt-2 btn btn-dark col-12 col-sm-12 col-md-3 col-lg-2">Edit</button>`;
+const buttonLogout = `<button id="btnlogout" type="button" onclick="logout()" class="mt-2 btn btn-dark col-12 col-sm-12 col-md-3 col-lg-2">Logout</button>`;
 const buttonFollow = `<button id="btnfollow" type="button" onclick="follow()" class="btn btn-dark col-12 col-sm-12 col-md-3 col-lg-2">Follow</button>`;
 const buttonUnfollow = `<button id="btnunfollow" type="button" onclick="unfollow()" class="btn btn-dark col-12 col-sm-12 col-md-3 col-lg-2">Unfollow</button>`;
 const main = document.querySelector("main");
@@ -169,8 +180,8 @@ axios.get('api-profile.php').then(response => {
         //Visualizza post
         const posts = response.data["posts"];
         main.innerHTML = generaUtente(response.data["info"], response.data["isMine"]);
-        for(let i=0; i<posts.length; i++){
-            posts[i]["liked"] = (posts[i]["liked"]==null) ? "fa-regular" : "fa-solid";
+        for (let i = 0; i < posts.length; i++) {
+            posts[i]["liked"] = (posts[i]["liked"] == null) ? "fa-regular" : "fa-solid";
             main.insertAdjacentHTML("beforeend", generaPost(posts[i]));
         }
     } else {
@@ -190,7 +201,7 @@ function follow() {
         if (!response.data["islogged"]) {
             window.location.replace("./index.php");
         } else {
-            if(response.data["updateSuccess"]){
+            if (response.data["updateSuccess"]) {
                 let buttonArea = document.getElementById("buttonArea");
                 buttonArea.removeChild(document.getElementById("btnfollow"));
                 buttonArea.insertAdjacentHTML("beforeend", buttonUnfollow);
@@ -206,7 +217,7 @@ function unfollow() {
         if (!response.data["islogged"]) {
             window.location.replace("./login.php");
         } else {
-            if(response.data["updateSuccess"]==1){
+            if (response.data["updateSuccess"] == 1) {
                 let buttonArea = document.getElementById("buttonArea");
                 buttonArea.removeChild(document.getElementById("btnunfollow"));
                 buttonArea.insertAdjacentHTML("beforeend", buttonFollow);
@@ -222,7 +233,7 @@ function getPosts() {
     axios.get('api-profile.php').then(response => {
         if (response.data["islogged"]) {
             const posts = response.data["posts"];
-            for(let i=0; i<posts.length; i++){
+            for (let i = 0; i < posts.length; i++) {
                 main.insertAdjacentHTML("beforeend", generaPost(posts[i]));
             }
         } else {
@@ -244,7 +255,7 @@ function getFollowers() {
             window.location.replace("./login.php");
         } else {
             const users = response.data["users"];
-            for(let i=0; i<users.length; i++){
+            for (let i = 0; i < users.length; i++) {
                 main.insertAdjacentHTML("beforeend", generaRiga(users[i]));
             }
         }
@@ -263,7 +274,7 @@ function getFollowed() {
             window.location.replace("./login.php");
         } else {
             const users = response.data["users"];
-            for(let i=0; i<users.length; i++){
+            for (let i = 0; i < users.length; i++) {
                 main.insertAdjacentHTML("beforeend", generaRiga(users[i]));
             }
         }
