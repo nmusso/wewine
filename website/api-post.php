3cce8d0740cb0b455a2809ff7ae429586d48a238
Alessandro Sciarrillo <alessandr.sciarrill2@studio.unibo.it>
2023-01-21 11:46:18 +0100

    fix diffTime

--- website/api-post.php
+++ website/api-post.php
@@ -13,6 +13,21 @@ if($dbh->login_check()){
         $result["postInfo"] = $dbh->getPostById($_SESSION["watchedPost"]);
         $result["getPost"] = true;
 
+        $diffTime="";
+        if($result["postInfo"][0]["DaysAgo"]==0){
+            if($result["postInfo"][0]["MinutesAgo"]==1){
+                $diffTime = $result["postInfo"][0]["MinutesAgo"]." minute ";
+            } 
+            else{
+                $diffTime = $result["postInfo"][0]["MinutesAgo"]." minutes ";
+            }
+        }elseif ($result["postInfo"][0]["DaysAgo"]==1){
+            $diffTime = $result["postInfo"][0]["DaysAgo"]." day ";
+        }else{
+            $diffTime = $result["postInfo"][0]["DaysAgo"]." days ";
+        }
+        $result["postInfo"][0]["diffTime"] = $diffTime;
+
         $result["postInfo"][0]["imgProfilo"] = UPLOAD_DIR . $result["postInfo"][0]["imgProfilo"];
 
         if (isset($result["postInfo"][0]["immagine"])) {
