b49011b431d65fa7a9a133d6cdd4c7ae2bf5edf0
nmusso <niccolo.mussoni@gmail.com>
2023-01-26 21:44:04 +0100

    remove hosted db

--- website/bootstrap.php
+++ website/bootstrap.php
@@ -4,9 +4,6 @@ define("UPLOAD_DIR", "./upload/");
 require_once("utils/functions.php");
 require_once("db/database.php");
 
-// LOCAL
 $dbh = new DatabaseHelper("localhost", "sec_user", "f&4@YXuC9$5C^nLQ", "social", 3306);
-// HOSTED
-//$dbh = new DatabaseHelper("sql11.freesqldatabase.com", "sql11591817", "K9lnnXrFuQ", "sql11591817", 3306);
 
 ?>
